1ad08fb9031deee5a3a6d13195dd432c134a326b
Clyde Alegro <clyde.alegro@gmail.com>
2012-04-23 17:04:24 +0800

    added repo

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/Users/clyde/Sites/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
